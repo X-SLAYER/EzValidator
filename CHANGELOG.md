7130e6ef64bb290716288cf44e073c4280c2a33e
X-SLAYER <slayertn1999@gmail.com>
2023-03-05 14:21:56 +0100

    fix example + prepare for publish

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,18 +1,29 @@
 ## 0.0.1
 
-* ez_validator
+- ez_validator
+
 ## 0.1.1
 
-* add filledForm option + add set custom locale errors
+- add filledForm option + add set custom locale errors
+
 ## 0.1.2
 
-* fix web plugin error + required custom messages
+- fix web plugin error + required custom messages
+
 ## 0.1.3
 
-* add new validations methods
+- add new validations methods
+
 ## 0.1.4
 
-* Fix languages toggle
+- Fix languages toggle
+
 ## 0.1.5
 
-* Add support to all platforms
+- Add support to all platforms
+
+## 0.2.1
+
+- Fix support for Desktop platforms
+- Fix typos
+- Remove unused files
--- lib/validator/ez_validator_builder.dart
+++ lib/validator/ez_validator_builder.dart
@@ -11,7 +11,7 @@ class EzValidator {
   });
 
   /// optional by default is `False`
-  /// if optional `True` the required validation with be ignored
+  /// if optional `True` the required validation will be ignored
   final bool optional;
   final List<StringValidationCallback> validations = [];
   static EzLocale globalLocale = const DefaultLocale();
@@ -42,51 +42,90 @@ class EzValidator {
 
   StringValidationCallback build() => _test;
 
-  EzValidator required([String? message]) => _add((v) => v == null || v.trim().isEmpty ? message ?? globalLocale.required() : null);
+  EzValidator required([String? message]) =>
+      _add((v) => v == null || v.trim().isEmpty
+          ? message ?? globalLocale.required()
+          : null);
 
-  EzValidator minLength(int minLength, [String? message]) => _add((v) => v!.length < minLength ? message ?? globalLocale.minLength(v, minLength) : null);
+  EzValidator minLength(int minLength, [String? message]) =>
+      _add((v) => v!.length < minLength
+          ? message ?? globalLocale.minLength(v, minLength)
+          : null);
 
-  EzValidator min(int min, [String? message]) => _add((v) => int.parse('$v') < min ? message ?? globalLocale.min('$v', min) : null);
+  EzValidator min(int min, [String? message]) => _add((v) =>
+      int.parse('$v') < min ? message ?? globalLocale.min('$v', min) : null);
 
-  EzValidator max(int min, [String? message]) => _add((v) => int.parse('$v') > min ? message ?? globalLocale.max('$v', min) : null);
+  EzValidator max(int min, [String? message]) => _add((v) =>
+      int.parse('$v') > min ? message ?? globalLocale.max('$v', min) : null);
 
-  EzValidator positive([String? message]) => _add((v) => int.parse('$v') < 0 ? message ?? globalLocale.positive(v!) : null);
+  EzValidator positive([String? message]) => _add(
+      (v) => int.parse('$v') < 0 ? message ?? globalLocale.positive(v!) : null);
 
-  EzValidator negative([String? message]) => _add((v) => int.parse('$v') > 0 ? message ?? globalLocale.negative(v!) : null);
+  EzValidator negative([String? message]) => _add(
+      (v) => int.parse('$v') > 0 ? message ?? globalLocale.negative(v!) : null);
 
-  EzValidator maxLength(int maxLength, [String? message]) => _add((v) => v!.length > maxLength ? message ?? globalLocale.maxLength(v, maxLength) : null);
+  EzValidator maxLength(int maxLength, [String? message]) =>
+      _add((v) => v!.length > maxLength
+          ? message ?? globalLocale.maxLength(v, maxLength)
+          : null);
 
-  EzValidator matches(String pattern, [String? message]) => _add((v) => RegExp(pattern).hasMatch(v!) ? null : message ?? globalLocale.matches(pattern, v));
+  EzValidator matches(String pattern, [String? message]) =>
+      _add((v) => RegExp(pattern).hasMatch(v!)
+          ? null
+          : message ?? globalLocale.matches(pattern, v));
 
-  EzValidator email([String? message]) => _add((v) => emailRegExp.hasMatch(v!) ? null : message ?? globalLocale.email(v));
+  EzValidator email([String? message]) => _add((v) =>
+      emailRegExp.hasMatch(v!) ? null : message ?? globalLocale.email(v));
 
-  EzValidator phone([String? message]) => _add((v) => !anyLetterExp.hasMatch(v!) && phoneRegExp.hasMatch(v.replaceAll(nonDigitsExp, '')) ? null : message ?? globalLocale.phoneNumber(v));
+  EzValidator phone([String? message]) =>
+      _add((v) => !anyLetterExp.hasMatch(v!) &&
+              phoneRegExp.hasMatch(v.replaceAll(nonDigitsExp, ''))
+          ? null
+          : message ?? globalLocale.phoneNumber(v));
 
-  EzValidator ip([String? message]) => _add((v) => ipv4RegExp.hasMatch(v!) ? null : message ?? globalLocale.ip(v));
+  EzValidator ip([String? message]) => _add(
+      (v) => ipv4RegExp.hasMatch(v!) ? null : message ?? globalLocale.ip(v));
 
-  EzValidator ipv6([String? message]) => _add((v) => ipv6RegExp.hasMatch(v!) ? null : message ?? globalLocale.ipv6(v));
+  EzValidator ipv6([String? message]) => _add(
+      (v) => ipv6RegExp.hasMatch(v!) ? null : message ?? globalLocale.ipv6(v));
 
-  EzValidator url([String? message]) => _add((v) => urlRegExp.hasMatch(v!) ? null : message ?? globalLocale.url(v));
+  EzValidator url([String? message]) => _add(
+      (v) => urlRegExp.hasMatch(v!) ? null : message ?? globalLocale.url(v));
 
-  EzValidator number([String? message]) => _add((v) => digitsExp.hasMatch(v!) ? null : message ?? globalLocale.number(v));
+  EzValidator number([String? message]) => _add(
+      (v) => digitsExp.hasMatch(v!) ? null : message ?? globalLocale.number(v));
 
-  EzValidator notNumber([String? message]) => _add((v) => nonDigitsExp.hasMatch(v!) ? null : message ?? globalLocale.notNumber(v));
+  EzValidator notNumber([String? message]) => _add((v) =>
+      nonDigitsExp.hasMatch(v!) ? null : message ?? globalLocale.notNumber(v));
 
-  EzValidator boolean([String? message]) => _add((v) => booleanExp.hasMatch(v!) ? null : message ?? globalLocale.boolean(v));
+  EzValidator boolean([String? message]) => _add((v) =>
+      booleanExp.hasMatch(v!) ? null : message ?? globalLocale.boolean(v));
 
-  EzValidator uuid([String? message]) => _add((v) => uuidExp.hasMatch(v!) ? null : message ?? globalLocale.uuid(v));
+  EzValidator uuid([String? message]) => _add(
+      (v) => uuidExp.hasMatch(v!) ? null : message ?? globalLocale.uuid(v));
 
-  EzValidator lowerCase([String? message]) => _add((v) => v == v!.toLowerCase() ? null : message ?? globalLocale.lowerCase(v));
+  EzValidator lowerCase([String? message]) => _add((v) =>
+      v == v!.toLowerCase() ? null : message ?? globalLocale.lowerCase(v));
+
+  EzValidator upperCase([String? message]) => _add((v) =>
+      v == v!.toUpperCase() ? null : message ?? globalLocale.upperCase(v));
+
+  EzValidator oneOf(List<String?> items, [String? message]) =>
+      _add((v) => items.contains(v)
+          ? null
+          : message ?? globalLocale.oneOf(items, v as String));
+
+  EzValidator notOneOf(List<String?> items, [String? message]) =>
+      _add((v) => !items.contains(v)
+          ? null
+          : message ?? globalLocale.notOneOf(items, v as String));
+
+  EzValidator defaultTest(String message, bool Function(String? v) test) =>
+      _add((v) => test(v) ? null : message);
 
   EzValidator date([String? message]) => _add(
-        (v) => DateTime.tryParse(v ?? '') != null ? null : message ?? globalLocale.date(v!),
+        (v) => DateTime.tryParse(v ?? '') != null
+            ? null
+            : message ?? globalLocale.date(v!),
       );
-
-  EzValidator upperCase([String? message]) => _add((v) => v == v!.toUpperCase() ? null : message ?? globalLocale.upperCase(v));
-
-  EzValidator oneOf(List<String?> items, [String? message]) => _add((v) => items.contains(v) ? null : message ?? globalLocale.oneOf(items, v as String));
-
-  EzValidator notOneOf(List<String?> items, [String? message]) => _add((v) => !items.contains(v) ? null : message ?? globalLocale.notOneOf(items, v as String));
-
-  EzValidator defaultTest(String message, bool Function(String? v) test) => _add((v) => test(v) ? null : message);
 }
