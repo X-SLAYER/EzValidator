356a15ccf6bb50973378c25bf8c8dc44d37602cf
X-SLAYER <slayertn1999@gmail.com>
2024-02-11 11:27:46 +0100

    add test cases for depends on method

--- test/depends_on_test.dart
+++ test/depends_on_test.dart
@@ -1,6 +1,8 @@
 import 'package:test/test.dart';
 import 'package:ez_validator/ez_validator.dart';
 
+import 'common/enums.dart';
+
 void main() {
   group('Schema Validation with dependsOn', () {
     final EzSchema schema = EzSchema.shape({
@@ -8,7 +10,6 @@ void main() {
       "stripe_account": EzValidator<String>().dependsOn(
         condition: (ref) => ref!["premium"]!,
         then: EzValidator<String>().required(),
-        orElse: EzValidator<String>(optional: true),
       ),
     });
 
@@ -35,6 +36,53 @@ void main() {
         "stripe_account": "valid_account",
       });
       expect(errors.isEmpty, isTrue);
+    });
+  });
+
+  group('Car Validation Schema Tests', () {
+    final EzSchema carValidationSchema = EzSchema.shape({
+      "car_type": EzValidator<CarType>(defaultValue: CarType.suv).required(),
+      "passangers_number": EzValidator<int>().dependsOn(
+        condition: (ref) => ref!["car_type"] == CarType.suv,
+        then: EzValidator<int>().required().max(6, 'Max 6 passangers'),
+        orElse: EzValidator<int>().required().max(4, 'Max 4 passangers'),
+      ),
+    });
+    test('SUV car with valid passengers number', () {
+      final errors = carValidationSchema.catchErrors({
+        "car_type": CarType.suv,
+        "passangers_number": 5,
+      });
+      expect(errors.isEmpty, isTrue);
+    });
+    test('SUV car with too many passangers', () {
+      final errors = carValidationSchema.catchErrors({
+        "car_type": CarType.suv,
+        "passangers_number": 7,
+      });
+      expect(errors['passangers_number'], equals('Max 6 passangers'));
+    });
+    test('Other car with too many passangers', () {
+      final errors = carValidationSchema.catchErrors({
+        "car_type": CarType.other,
+        "passangers_number": 7,
+      });
+      expect(errors['passangers_number'], equals('Max 4 passangers'));
+    });
+    test('Other car with valid passangers', () {
+      final errors = carValidationSchema.catchErrors({
+        "car_type": CarType.other,
+        "passangers_number": 3,
+      });
+      expect(errors['passangers_number'], isNull);
+    });
+
+    test('Other car with exact needed passangers', () {
+      final errors = carValidationSchema.catchErrors({
+        "car_type": CarType.other,
+        "passangers_number": 4,
+      });
+      expect(errors['passangers_number'], isNull);
     });
   });
 }
